adcd995033f222644e31b0fd2bce58192b7e8f78
Android Studio <devandroid@ort>
2024-07-31 08:24:30 -0300

    no anda la Home

--- src/app/Components/navbar.js
+++ src/app/Components/navbar.js
@@ -6,10 +6,10 @@ const Navbar = () => {
         <div className={styles.navbar}>
             <h2>Nav</h2>
             <ul>
-                <li><Link href="./">Home</Link></li>
-                <li><Link href="./reservas">Reservas</Link></li>
-                <li><Link href="./quienes-somos">Creditos</Link></li>
-                <li><Link href="./contactos">Ayuda</Link></li>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/reservas">Reservas</Link></li>
+                <li><Link href="/quienes-somos">Creditos</Link></li>
+                <li><Link href="/contactos">Ayuda</Link></li>
             </ul>
         </div>
     );
